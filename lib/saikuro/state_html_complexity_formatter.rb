b3cdb8b72ae1a0fcb58d73b52bd4a167b0b09903
wata727 <watassbass@gmail.com>
2016-08-07 17:23:10 +0900

    add first_line value in ParseDef and ParseClass

--- lib/saikuro/state_html_complexity_formatter.rb
+++ lib/saikuro/state_html_complexity_formatter.rb
@@ -14,14 +14,15 @@ class StateHTMLComplexityFormater < ParseStateFormater
     @out.puts "</html>"
   end
 
-  def start_class_compute_state(type_name,name,complexity,lines)
+  def start_class_compute_state(type_name,name,complexity,first_line,lines)
     @current = name
     @out.puts "<div class=\"class_complexity\">"
     @out.puts "<h2 class=\"class_name\">#{type_name} : #{name}</h2>"
+    @out.puts "<div class=\"class_first_line\">First Line: #{first_line}</div>"
     @out.puts "<div class=\"class_total_complexity\">Total Complexity: #{complexity}</div>"
     @out.puts "<div class=\"class_total_lines\">Total Lines: #{lines}</div>"
     @out.puts "<table width=\"100%\" border=\"1\">"
-    @out.puts "<tr><th>Method</th><th>Complexity</th><th># Lines</th></tr>"
+    @out.puts "<tr><th>Method</th><th>Complexity</th><th>First Line</th><th># Lines</th></tr>"
   end
 
   def end_class_compute_state(name)
@@ -29,10 +30,10 @@ class StateHTMLComplexityFormater < ParseStateFormater
     @out.puts "</div>"
   end
 
-  def def_compute_state(name, complexity, lines)
+  def def_compute_state(name, complexity, first_line, lines)
     return if @filter.ignore?(complexity)
     klass = warn_error?(complexity, name)
-    @out.puts "<tr><td>#{name}</td><td#{klass}>#{complexity}</td><td>#{lines}</td></tr>"
+    @out.puts "<tr><td>#{name}</td><td#{klass}>#{complexity}</td><td>#{first_line}</td><td>#{lines}</td></tr>"
   end
 
 end
